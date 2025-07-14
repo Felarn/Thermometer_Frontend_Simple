f3f66e5479ed7d259bd8670f22982cc086a3ec56
Felarn <120136549+Felarn@users.noreply.github.com>
2025-07-14 13:21:30 +0300

    ADD data decemation and max, min, mean & median

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ console.log('script loaded');
 
 let chart = null;
 // Пример данных
+const pointsTarget = 1000;
+
+const median = (arr) => arr.sort().at(Math.floor(arr.length / 2));
+
+const decemate = (data) => {
+  const dataLen = data.temp.length;
+  console.log('DEC' + dataLen);
+  const step = Math.floor(dataLen / pointsTarget);
+  const decematedData = { time: [], min: [], median: [], max: [], mean: [] };
+  for (let i = 0; i < dataLen; i += step) {
+    const slice = data.temp.slice(i, i + step);
+    decematedData.time.push(data.time.at(i));
+    decematedData.max.push(Math.max(...slice));
+    decematedData.median.push(median(slice));
+    decematedData.mean.push(
+      slice.reduce((acc, item) => acc + item, 0) / slice.length
+    );
+    decematedData.min.push(Math.min(...slice));
+  }
+
+  return decematedData;
+};
 
 const doShit = (data) => {
+  console.log(data.time.length);
+  // const timeData = data.time.slice(-100);
+  // const tempData = data.temp.slice(-100);
   const timeData = data.time;
-  const tempData = data.temp;
-  const minTemp = Math.min(data.temp);
-  const maxTemp = Math.max(data.temp);
+  const dataMin = data.min;
+  const dataMean = data.mean;
+  const dataMedian = data.median;
+  const dataMax = data.max;
+  const minTemp = Math.min(data.temp) - 2;
+  const maxTemp = Math.max(data.temp) + 2;
 
   // Конвертация времени в читаемый формат
   const formatDate = (timestamp) => {
@@ -35,14 +63,48 @@ const doShit = (data) => {
       ),
       datasets: [
         {
-          label: 'Температура (°C)',
-          data: tempData,
-          borderColor: 'rgb(255, 99, 132)',
-          backgroundColor: 'rgba(255, 99, 132, 0.1)',
+          lineTension: 0,
+          label: 'среднее (°C)',
+          data: dataMean,
+          borderColor: 'rgba(230, 181, 22, 1)',
+          backgroundColor: 'rgba(230, 181, 22, 1)',
+          borderWidth: 4,
+          pointRadius: 0,
+          fill: false,
+          tension: 0,
+        },
+        {
+          lineTension: 0,
+          label: 'Медиана (°C)',
+          data: dataMedian,
+          borderColor: 'rgba(79, 221, 44, 1)',
+          backgroundColor: 'rgba(79, 221, 44, 1)',
           borderWidth: 2,
-          pointRadius: 4,
-          fill: true,
-          tension: 0.3,
+          pointRadius: 0,
+          fill: false,
+          tension: 0,
+        },
+        {
+          lineTension: 0,
+          label: 'Минимум (°C)',
+          data: dataMin,
+          borderColor: 'rgba(44, 180, 221, 1)',
+          backgroundColor: 'rgba(44, 180, 221, 0.1)',
+          borderWidth: 2,
+          pointRadius: 0,
+          fill: false,
+          tension: 0,
+        },
+        {
+          lineTension: 0,
+          label: 'Максимум (°C)',
+          data: dataMax,
+          borderColor: 'rgba(255, 0, 0, 1)',
+          backgroundColor: 'rgba(255, 0, 0, 0.1)',
+          borderWidth: 2,
+          pointRadius: 0,
+          fill: '-1',
+          tension: 0,
         },
       ],
     },
@@ -72,8 +134,8 @@ const doShit = (data) => {
               weight: 'bold',
             },
           },
-          suggestedMin: minTemp - 2,
-          suggestedMax: maxTemp + 2,
+          suggestedMin: minTemp,
+          suggestedMax: maxTemp,
         },
       },
       plugins: {
@@ -100,6 +162,10 @@ const doShit = (data) => {
 const refreshData = () => {
   fetch('https://felarn.fun')
     .then((resp) => resp.json())
+    // .then((data) =>
+    //   decemate({ time: data.time.slice(-1000), temp: data.temp.slice(-1000) })
+    // )
+    .then(decemate)
     .then(doShit)
     .then(() => setTimeout(refreshData, 1000));
 };
